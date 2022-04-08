9afdcd63e7500377ed78f62d6b047a67a775a2f3
sadibaba2000 <77102348+sadibaba2000@users.noreply.github.com>
2022-04-08 20:32:25 +0200

    Create Controllers

---
+++ FullStackApi/Controllers/CompanyController.cs
@@ -0,0 +1,18 @@
+#nullable disable
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FullStackApi.Models;
+
+namespace FullStackApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CompanyController : ControllerBase
+{
+    private readonly FullStackContext _context;
+
+    public CompanyController(FullStackContext context)
+    {
+        _context = context;
+    }
+}
---
+++ FullStackApi/Controllers/DeveloperController.cs
@@ -0,0 +1,18 @@
+#nullable disable
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using FullStackApi.Models;
+
+namespace FullStackApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DeveloperController : ControllerBase
+{
+    private readonly FullStackContext _context;
+
+    public DeveloperController(FullStackContext context)
+    {
+        _context = context;
+    }
+}
