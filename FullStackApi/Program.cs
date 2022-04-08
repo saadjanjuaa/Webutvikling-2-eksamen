bef90bb00929511e0e7480b496bb6e6165b94fde
sadibaba2000 <77102348+sadibaba2000@users.noreply.github.com>
2022-04-08 20:27:16 +0200

    Update Program.cs

--- FullStackApi/Program.cs
+++ FullStackApi/Program.cs
@@ -1,6 +1,10 @@
+using FullStackApi.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<FullStackContext>(options => options.UseSqlite("Data Source=FullStack.db"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
