2d18b7c54fb5e19cc2cc3f4f0813110b78bcc78b
sadibaba2000 <77102348+sadibaba2000@users.noreply.github.com>
2022-04-09 15:48:05 +0200

    Added CORS to Program.cs

--- FullStackApi/Program.cs
+++ FullStackApi/Program.cs
@@ -6,12 +6,27 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<FullStackContext>(options => options.UseSqlite("Data Source=FullStack.db"));
 
+
+builder.Services.AddCors(
+    options => {
+        options.AddPolicy("AllowAll",
+            builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+        );
+    }
+);
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
