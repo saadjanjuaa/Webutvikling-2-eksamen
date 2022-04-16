0a66ca9fde64c40d6c3118d08142d41a1749749a
sadibaba2000 <77102348+sadibaba2000@users.noreply.github.com>
2022-04-16 14:02:31 +0200

    Add SearchCompanyItem.vue & added some images

---
+++ full-stack-vue/src/components/companies/SearchCompanyItem.vue
@@ -0,0 +1,21 @@
+<template>
+    <section class="mt-5">
+
+        <h3>Søk på id</h3>
+
+        <label class="mb-1">Angi id</label>
+        <div class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Id" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </div>
+
+    </section>
+</template>
+
+<style scoped>
+
+    input {
+        width: 13rem;
+    }
+    
+</style>
