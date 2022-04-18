f0fbdbf21056f3eb68319c05a1f8131c0a4c3a10
sadibaba2000 <77102348+sadibaba2000@users.noreply.github.com>
2022-04-18 15:03:55 +0200

    Add icons to PageHeader

--- full-stack-vue/src/components/shared/PageHeader.vue
+++ full-stack-vue/src/components/shared/PageHeader.vue
@@ -6,10 +6,10 @@
         </router-link>
 
         <ul class="nav ms-auto me-4 ">
-            <li class="nav-item"><router-link class="nav-link" to="/">Hjem</router-link></li> 
-            <li class="nav-item"><router-link class="nav-link" to="/assignments">Oppdrag</router-link></li>
-            <li class="nav-item"><router-link class="nav-link" to="/companies">Firmaer</router-link></li>
-            <li class="nav-item"><router-link class="nav-link" to="/developers">Utviklere</router-link></li>
+            <li class="nav-item"><router-link class="nav-link" to="/">Hjem<i class=" ms-1 bi bi-house-door-fill"></i></router-link></li> 
+            <li class="nav-item"><router-link class="nav-link" to="/assignments">Oppdrag<i class="ms-1 bi bi-clipboard2-data-fill"></i></router-link></li>
+            <li class="nav-item"><router-link class="nav-link" to="/companies">Firmaer<i class="ms-1 bi bi-building"></i></router-link></li>
+            <li class="nav-item"><router-link class="nav-link" to="/developers">Utviklere <i class="ms-1 bi bi-people-fill"></i></router-link></li>
         </ul>
 
     </nav>
